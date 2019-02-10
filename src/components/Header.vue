63bda5dd47794f86239b3e629946e3e31f9a575e
Giovanni Pizelli <giovannipizelli@gmail.com>
2019-02-10 15:06:24 -0200

    Parei na aula 309

---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-toolbar app>
+        <v-toolbar-title class="headline text-uppercase mr-4">
+            <span>Stock</span>
+            <span class="font-weight-light">Trader</span>
+        </v-toolbar-title>
+        <v-toolbar-items>
+            <v-btn flat to="/">Início</v-btn>
+            <v-btn flat to="/portfolio">Portfólio</v-btn>
+            <v-btn flat to="/stocks">Stocks</v-btn>
+        </v-toolbar-items>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+            <v-btn flat> Finalizar Dia</v-btn>
+            <v-menu offset-y>
+                <v-btn flat slot="activator">Salvar & Carregar</v-btn>
+                <v-list>
+                    <v-list-tile>
+                        <v-list-tile-title>Salvar Dados</v-list-tile-title>
+                    </v-list-tile>
+                    <v-list-tile>
+                        <v-list-tile-title>Carregar Dados</v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-menu>
+            <v-layout align-center>
+                <span class="text-uppercase grey--text text--darken-2">
+                    Saldo: {{ funds }}
+                </span>
+            </v-layout>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+    computed:{
+        funds(){
+            return this.$store.getters.funds
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.id" :stock="stock" />
+        <v-layout v-if="stocks.length < 1" row justify-center="">
+            <v-card>
+                <v-card-text>
+                    Nenhuma ação até o momento!
+                </v-card-text>
+            </v-card>
+        </v-layout>
+    </v-layout>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+import Stock from './Stock'
+
+export default {
+    components: { Stock },
+    computed: {
+        ...mapGetters({stocks: 'stockPortfolio'})
+    }
+
+}
+</script>
+
+<style>
+
+</style>
