129f55ca59fbf7971df1806da2640567e6915826
Giovanni Pizelli <giovannipizelli@gmail.com>
2019-03-22 15:27:24 -0300

    Projeto finalizado

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,21 +11,21 @@
         </v-toolbar-items>
         <v-spacer></v-spacer>
         <v-toolbar-items>
-            <v-btn flat> Finalizar Dia</v-btn>
+            <v-btn flat @click="endDay"> Finalizar Dia</v-btn>
             <v-menu offset-y>
                 <v-btn flat slot="activator">Salvar & Carregar</v-btn>
                 <v-list>
-                    <v-list-tile>
+                    <v-list-tile @click="saveData">
                         <v-list-tile-title>Salvar Dados</v-list-tile-title>
                     </v-list-tile>
-                    <v-list-tile>
+                    <v-list-tile @click="loadDataLocal">
                         <v-list-tile-title>Carregar Dados</v-list-tile-title>
                     </v-list-tile>
                 </v-list>
             </v-menu>
             <v-layout align-center>
                 <span class="text-uppercase grey--text text--darken-2">
-                    Saldo: {{ funds }}
+                    Saldo: {{ funds | currency }}
                 </span>
             </v-layout>
         </v-toolbar-items>
@@ -33,10 +33,25 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
     computed:{
         funds(){
             return this.$store.getters.funds
+        }
+    },
+    methods: {
+        ...mapActions(['randomizeStocks', 'loadData']),
+        endDay() {
+            this.randomizeStocks()
+        },
+        saveData() {
+            const {funds, stockPortfolio, stocks} = this.$store.getters
+            this.$http.put('data.json', { funds, stockPortfolio, stocks })
+        },
+        loadDataLocal() {
+            this.loadData()
         }
     }
 }
--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 
+import './plugins/axios'
+
 import router from './router'
 import store from './store/store'
 
 Vue.config.productionTip = false
+
+Vue.filter('currency', value => `R$ ${value.toLocaleString()}`)
 
 new Vue({
 	store,
